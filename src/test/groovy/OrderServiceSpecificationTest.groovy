8f85b171eaf1b9dff32835dc858e10f18d171c96
Mateusz <borek.web1@gmail.com>
2024-11-22 16:03:16 +0100

    Fix tests for OrderService

--- src/test/groovy/OrderServiceSpecificationTest.groovy
+++ src/test/groovy/OrderServiceSpecificationTest.groovy
@@ -20,7 +20,7 @@ class OrderServiceSpecificationTest extends Specification {
 
     def controller = new OrderController(manipulateOrderUseCase, queryOrderUseCase, userSecurity)
 
-    def "user can create new order - fix"() {
+    def "user can create new order"() {
 
         given:
         Recipient recipient = new Recipient("arek@op.pl", "Arek", "123456", "Nowa", "00-987", "Wozy")
@@ -43,70 +43,16 @@ class OrderServiceSpecificationTest extends Specification {
         RequestContextHolder.setRequestAttributes(attributes)
 
         def eitherResponse = new PlaceOrderResponse(true, null, expectedOrderId)
+        manipulateOrderUseCase.placeOrder(placeOrderCommand) >> eitherResponse
 
         when:
         ResponseEntity<Object> response = controller.createOrder(placeOrderCommand)
 
         then:
-        1 * manipulateOrderUseCase.placeOrder(placeOrderCommand) >> eitherResponse
         response.statusCode == HttpStatus.CREATED
         response.headers.getLocation().toString() == "http://localhost:8080/orders/${expectedOrderId}"
 
         cleanup:
         RequestContextHolder.resetRequestAttributes()
     }
-
-//    def "user can create new order"() {
-//
-//        given:
-//        OrderItemCommand orderItemCommand1 = new OrderItemCommand(1L, 2)
-//        OrderItemCommand orderItemCommand2 = new OrderItemCommand(12L, 5)
-//        Recipient recipient = new Recipient("arek@op.pl", "Arek", "123456", "Nowa", "00-987", "Wozy")
-//
-//        PlaceOrderCommand placeOrderCommand = PlaceOrderCommand.builder()
-//                .items(List.of(orderItemCommand1, orderItemCommand2))
-//                .recipient(recipient)
-//                .build()
-//
-//        def expectedOrderId = 123L
-//
-//        URI expectedURI = new URI("orders")
-//
-//        MockHttpServletRequest request = new MockHttpServletRequest("POST", "/orders")
-//        request.setServerName("localhost")
-//        request.setServerPort(8080)
-//        request.setScheme("http")
-//
-//        //request.setRequestURI(expectedURI.toString())
-//        //request.setRequestURI("http://localhost/orders/$expectedOrderId")
-//        //now ServletRequestAttributes attributes = new ServletRequestAttributes(request)
-//        //nowRequestContextHolder.setRequestAttributes(attributes)
-//        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request))
-//        MockHttpServletResponse response = new MockHttpServletResponse();
-//        GroovySpy(ServletUriComponentsBuilder, global: true)
-//        def mockUriComponentsBuilder = Mock(ServletUriComponentsBuilder)
-////        mockUriComponentsBuilder.path(_) >> mockUriComponentsBuilder
-////        mockUriComponentsBuilder.build() >> expectedURI.toString()
-//
-//        ServletUriComponentsBuilder.fromCurrentRequestUri().path("/") >> mockUriComponentsBuilder
-////        CreatedURI createdURI = new CreatedURI("/orders/123")
-////        URI uri2 = createdURI.uri()
-////        expectedURI.toString() == "http://localhost/some/request/uri/orders/123"
-////        expectedURI.toString() == "/orders/$expectedOrderId"
-//
-//
-//        def eitherResponse = new PlaceOrderResponse(true, null, expectedOrderId)
-//        //controller.orderUri(expectedOrderId) >> expectedURI.toString()
-//
-//        when:
-//        //nowResponseEntity<Object> response = controller.createOrder(placeOrderCommand)
-//
-//        then:
-//        1 * manipulateOrderUseCase.placeOrder(placeOrderCommand) >> eitherResponse
-//        //controller.orderUri(expectedOrderId) >> expectedURI.toString()
-//        response.statusCode == HttpStatus.CREATED
-//        //response.headers.location == expectedURI.toString()
-//        //response.headers.location == new URI("http://localhost/orders/$expectedOrderId")
-//        response.headers.location == expectedURI
-//    }
 }
